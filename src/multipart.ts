cbbf1f0a753e64a2f9bc5576f901c990e19c4aff
Tom Sherman <the.tomsherman@gmail.com>
2022-06-21 18:34:06 +0100

    WIP stream parser

--- src/multipart.ts
+++ src/multipart.ts
@@ -1,18 +1,42 @@
 import { Field } from "./field.js";
-import { indexOfNeedleBytes } from "./util.js";
+import {
+  concatBytes,
+  indexOfNeedleBytes,
+  streamAsyncIterator,
+} from "./util.js";
 
+// TODO: Support other charsets
 const encoder = new TextEncoder();
 
 const BOUNDARY_EQUAL = encoder.encode("boundary=");
+const RETURN_NEWLINE_2 = encoder.encode("\r\n\r\n");
+const DASHSASH = encoder.encode("--");
 
-interface MultipartInit {
+type StreamState =
+  | {
+      value: "consumingStartBoundary";
+      startBoudaryBytes: Uint8Array;
+    }
+  | {
+      value: "consumingEndBoundary";
+      endBoundaryBytes: Uint8Array;
+    }
+  | {
+      value: "readingHeaderPart";
+      headerBytes: Uint8Array;
+    }
+  | {
+      value: "readingBodyPart";
+      headers: Headers;
+      initialBodyBytes: Uint8Array;
+    };
+
+export interface MultipartInit {
   body: ReadableStream<Uint8Array> | null;
   headers: Headers;
 }
 
-export async function* multipart(
-  input: MultipartInit
-): AsyncGenerator<Field, void, Field> {
+export function multipart(input: MultipartInit): AsyncIterator<Field> {
   const body = input.body;
   if (!body) {
     throw new Error("Failed to fetch");
@@ -28,12 +52,84 @@ export async function* multipart(
     throw new Error("Failed to fetch");
   }
 
+  const startBoundaryBytes = concatBytes(DASHSASH, boundaryByte);
+  const endBoundaryBytes = concatBytes(startBoundaryBytes, DASHSASH);
+
+  let state: StreamState = {
+    value: "consumingStartBoundary",
+    startBoudaryBytes: new Uint8Array(),
+  };
+
   const stream = new TransformStream<Uint8Array, Field>({
-    transform: (chunk, controller) => {},
+    transform: (chunk, controller) => {
+      switch (state.value) {
+        case "consumingStartBoundary": {
+          const newBoundaryBytes = concatBytes(state.startBoudaryBytes, chunk);
+          const headersStartIndex = indexOfNeedleBytes(
+            newBoundaryBytes,
+            startBoundaryBytes
+          );
+
+          if (headersStartIndex === -1) {
+            state.startBoudaryBytes = newBoundaryBytes;
+            return;
+          }
+
+          const headerBytes = newBoundaryBytes.slice(
+            0,
+            headersStartIndex + startBoundaryBytes.byteLength
+          );
+
+          state = {
+            value: "readingHeaderPart",
+            headerBytes,
+          };
+
+          return;
+        }
+
+        case "readingHeaderPart": {
+          const newHeaderBytes = concatBytes(state.headerBytes, chunk);
+          const contentStartIndex = indexOfNeedleBytes(
+            newHeaderBytes,
+            RETURN_NEWLINE_2
+          );
+          if (contentStartIndex === -1) {
+            state.headerBytes = newHeaderBytes;
+            return;
+          }
+
+          const headerBytes = newHeaderBytes.slice(0, contentStartIndex);
+          const bodyBytes = newHeaderBytes.slice(
+            contentStartIndex + RETURN_NEWLINE_2.byteLength
+          );
+
+          state = {
+            value: "readingBodyPart",
+            headers: parseHeaderBytes(headerBytes),
+            initialBodyBytes: bodyBytes,
+          };
+          return;
+        }
+
+        case "readingBodyPart": {
+          // Tee the current stream to a and b
+          // Transform a so that it closes on endboundary
+          // let a be the Field body
+          // yield Field using state.headers
+          // let b be the rest of the stream
+        }
+
+        default: {
+          throw new Error("Unexpected state");
+        }
+      }
+    },
   });
+
   body.pipeTo(stream.writable);
 
-  // TODO: Draw the rest of the owl
+  return streamAsyncIterator(stream.readable);
 }
 
 function getBoundary(contentType: string): Uint8Array | undefined {
@@ -48,4 +144,7 @@ function getBoundary(contentType: string): Uint8Array | undefined {
   } else {
     return undefined;
   }
+}
+function parseHeaderBytes(headerBytes: Uint8Array): Headers {
+  throw new Error("Function not implemented.");
 }
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,23 @@
+export async function* streamAsyncIterator<T>(
+  stream: ReadableStream<T>
+): AsyncGenerator<T, void> {
+  const reader = stream.getReader();
+
+  try {
+    while (true) {
+      const { done, value } = await reader.read();
+
+      if (done) {
+        return;
+      }
+
+      yield value;
+    }
+  } finally {
+    reader.releaseLock();
+  }
+}
+
 export function concatBytes(a: Uint8Array, b: Uint8Array) {
   const c = new Uint8Array(a.length + b.length);
   c.set(a);
